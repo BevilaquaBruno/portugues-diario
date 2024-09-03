e80e94d9b5b3ebf2134598160e8bb9e18722bd70
Bruno Bevilaqua <bbbevilaqua@gmail.com>
2024-09-03 12:25:05 -0300

    add wildcards validations

--- src/tips/tips.controller.ts
+++ src/tips/tips.controller.ts
@@ -22,22 +22,22 @@ export class TipsController {
     return this.tipsService.findOne(+id);
   }
 
-  @Patch(':id')
+  @Patch(':id(\\d+)')
   update(@Param('id') id: string, @Body() updateTipDto: UpdateTipDto) {
     return this.tipsService.update(+id, updateTipDto);
   }
 
-  @Delete(':id')
+  @Delete(':id(\\d+)')
   remove(@Param('id') id: string) {
     return this.tipsService.remove(+id);
   }
 
-  @Get('/like/:id')
+  @Get('/like/:id(\\d+)')
   likeTip(@Param('id') id: string) {
     return this.tipsService.likeTip(+id);
   }
 
-  @Get('/dislike/:id')
+  @Get('/dislike/:id(\\d+)')
   dislikeTip(@Param('id') id: string) {
     return this.tipsService.dislikeTip(+id);
   }
