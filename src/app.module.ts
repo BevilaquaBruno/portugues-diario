5b525cb3cb951ec4f4d3470ad2c7f2d0b341b7d9
Bruno Bevilaqua <bbbevilaqua@gmail.com>
2024-09-03 08:35:23 -0300

    remove base controllers

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { TipsModule } from './tips/tips.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { config } from './ormconfig';
 
 @Module({
   imports: [TipsModule, TypeOrmModule.forRoot(config)],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
