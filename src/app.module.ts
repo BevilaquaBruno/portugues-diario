ac402e508128fab026af0d3d357f1f80bdedf32a
Bruno Bevilaqua <bbbevilaqua@gmail.com>
2024-09-26 10:25:02 -0300

    cache implemented

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,9 +7,10 @@ import { AuthModule } from './auth/auth.module';
 import { AppController } from './app.controller';
 import { UsuariosModule } from './usuarios/usuarios.module';
 import { DicasModule } from './dicas/dicas.module';
+import { CacheModule } from '@nestjs/cache-manager';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(config), TipsModule, UserModule, AuthModule, UsuariosModule, DicasModule],
+  imports: [TypeOrmModule.forRoot(config), TipsModule, UserModule, AuthModule, UsuariosModule, DicasModule, CacheModule.register()],
   controllers: [AppController]
 })
 
