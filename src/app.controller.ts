901f2ed8b4bcece3c7c1948dacd34227403dbe9d
Bruno Fernando Bevilaqua <bbbevilaqua@gmail.com>
2024-10-03 23:15:41 -0300

    update css for login form

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,16 +1,18 @@
-import { Controller, Get, Inject, Res } from "@nestjs/common";
-import { returnDbDateFormatted } from "./helper";
-import { TipsService } from "./tips/tips.service";
-import { Response } from "express";
-import { CACHE_MANAGER } from "@nestjs/cache-manager";
+import { Controller, Get, Inject, Res } from '@nestjs/common';
+import { returnDbDateFormatted } from './helper';
+import { TipsService } from './tips/tips.service';
+import { Response } from 'express';
+import { CACHE_MANAGER } from '@nestjs/cache-manager';
 import { Cache } from 'cache-manager';
 
 @Controller()
 export class AppController {
+  constructor(
+    private readonly tipsService: TipsService,
+    @Inject(CACHE_MANAGER) private cacheManager: Cache,
+  ) {}
 
-  constructor(private readonly tipsService: TipsService, @Inject(CACHE_MANAGER) private cacheManager: Cache) { }
-
-    /*
+  /*
     @Get('/changeCacheDate')
     async changeCacheDate(){
       let todayTip;
@@ -23,36 +25,28 @@ export class AppController {
     }
     */
 
-
   @Get('/')
   async todayTip(@Res() res: Response) {
     try {
-
       let todayTip;
       //pega a dica do cache atual
       todayTip = await this.cacheManager.get('todayTip');
-  
+
       //se não existir a dica no cache, pega uma do banco
-      if (undefined == todayTip)
-        todayTip = await this.returnTodayTip();
-  
-  
+      if (undefined == todayTip) todayTip = await this.returnTodayTip();
+
       // pega as duas datas no formato YYYY-MM-DD
       let todayDate = returnDbDateFormatted(new Date());
       let tipDate = returnDbDateFormatted(new Date(todayTip.showed_in_date));
-  
+
       // se a data de hoje for diferente da data da dica, pega do banco
-      if(todayDate != tipDate)
-        todayTip = await this.returnTodayTip();
+      if (todayDate != tipDate) todayTip = await this.returnTodayTip();
 
       // Retorna a dica
-      return res.render(
-        'tip/tip',
-        {
-          layout: 'layouts/main-layout',
-          ...todayTip
-        }
-      );
+      return res.render('tip/tip', {
+        layout: 'layouts/main-layout',
+        ...todayTip,
+      });
     } catch (error) {
       // deu alguma merda
       return res.render('errors/500');
@@ -64,7 +58,6 @@ export class AppController {
     return res.render('login', { layout: 'layouts/login-layout' });
   }
 
-
   private async returnTodayTip() {
     //cria a variavel para a dica
     let todayTip;
@@ -72,6 +65,7 @@ export class AppController {
     let today = new Date();
     // Converte a data para o formato YYYY-MM-DD
     let todayDate = returnDbDateFormatted(today);
+
     // Procura no banco de dados uma dica com a data atual preenchida
     let todayTipByDate = await this.tipsService.getTodayTip(todayDate);
 
@@ -85,8 +79,9 @@ export class AppController {
 
       // Converte a dica consultada agora na dica do dia
       todayTip = tipWithoutDate;
+
       // Seta a data da dica para a data atual criada antes
-      todayTip.showed_in_date = today;
+      todayTip.showed_in_date = todayDate;
 
       // Atualiza a data da dica de hoje no banco de dados
       await this.tipsService.update(todayTip.id, todayTip);
